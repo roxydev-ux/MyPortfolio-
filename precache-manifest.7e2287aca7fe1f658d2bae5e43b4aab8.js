5373f5ca489e5a63ff3dde6687cd29d5e82fef27
Talshajar <rocio.chavoya@gmail.com>
2019-06-06 17:29:38 -0500

    Updates

---
+++ precache-manifest.7e2287aca7fe1f658d2bae5e43b4aab8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9d0b6523183903ecdfa3a09ff0ba57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3addb6d153ea373e5cdf",
+    "url": "/static/css/main.044eedc1.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3addb6d153ea373e5cdf",
+    "url": "/static/js/main.9a246444.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e9c3f448eca582f83c14704100ab57d",
+    "url": "/static/media/devicons.1e9c3f44.svg"
+  },
+  {
+    "revision": "36c5ab9318386dc0499a0d89691e6331",
+    "url": "/static/media/devicons.36c5ab93.eot"
+  },
+  {
+    "revision": "5ed4d6468a5a0f3cf9c82f854936feab",
+    "url": "/static/media/devicons.5ed4d646.ttf"
+  },
+  {
+    "revision": "a8639dec5833a8375d22fe425b01f4eb",
+    "url": "/static/media/devicons.a8639dec.woff"
+  },
+  {
+    "revision": "74f7cdffc9b2c3f62771c7acac3f512d",
+    "url": "/static/media/grey.74f7cdff.png"
+  },
+  {
+    "revision": "286c87cf2b62fab27f8c8e0ebf70c9fe",
+    "url": "/static/media/scenic_view_of_foggy_forest.286c87cf.jpeg"
+  }
+]);
